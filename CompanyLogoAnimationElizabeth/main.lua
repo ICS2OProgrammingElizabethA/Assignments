8dc2d1639370f7f7fb22192fea504abb76d4eb85
ElizabethAcheng <43419208+ElizabethAcheng@users.noreply.github.com>
2018-11-09 14:33:02 -0500

    added company logo animation

--- CompanyLogoAnimationElizabeth/main.lua
+++ CompanyLogoAnimationElizabeth/main.lua
@@ -1,7 +1,39 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
+--Title: Company Logo
+--Name: Elizabeth
+-- Course: ICS2O/3C
+--This program displays an image of our Company Logo across th screen.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--global variables
+local scrollSpeedBanana = 3
+
+--set the background colour
+display.setDefault( "background", 195/255, 20/255, 165/255 )
+
+--character images with width and height
+local banana = display.newImageRect("Images/CompanyLogoElizabeth@2x copy.png", 200, 400)
+
+--set the image to be transparent
+realCat.alpha = 1
+
+--set the initial x and y position of banana
+realCat.x = 0
+realCat.y = display.contentHeight / 3
+
+--Function: MoveRealcat
+--Input: this function accepts an event listener
+--Output: none
+--Description: This function adds the scroll speed to the x-value of the ship
+local function MoveBanana(event)
+	--add the scroll speed to the x-value of the ship
+	realCat.x = realCat.x + scrollSpeedCat
+
+	--change the transparency of the ship every time it so fast that it fades out
+	realCat.alpha = realCat.alpha - 0.000002/2
+end
+
+--MoveRealCat,MoveDog and MoveRocket will be called over and over again
+Runtime:addEventListener("enterFrame", MoveBanana)
